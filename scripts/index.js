630dd29858af1f6628eb2cc76074b10dddb5d84f
DEMETRIUS RODRIGUEZ <dmrodriguez2019@gmail.com>
2025-05-23 15:59:14 -0400

    modal submmison and css

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,30 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editprofileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editprofileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostbtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+const addCardFormElement = document.querySelector(
+  "#new-post-modal .modal__form"
+);
+const nameInput = addCardFormElement.querySelector("#card-caption-input");
+const linkInput = addCardFormElement.querySelector("#card-image-input");
+
 editProfileBtn.addEventListener("click", function () {
+  editprofileNameInput.value = profileNameEl.textContent;
+  editprofileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +39,20 @@ newPostbtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editprofileNameInput.value;
+  profileDescriptionEl.textContent = editprofileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log("Caption:", nameInput.value);
+  console.log("Image link:", linkInput.value);
+  document.querySelector("#new-post-modal").classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileFormSubmit);
+addCardForm.addEventListener("submit", handleAddCardFormSubmit);
